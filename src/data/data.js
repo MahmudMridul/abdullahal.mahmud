0db9ae650f2fb2bf0d982560ee8acb23bbaa9afe
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-20 17:32:48 +0600

    Add new data file

---
+++ src/data/data.js
@@ -0,0 +1,89 @@
+export const basicInfo = {
+   name: "Abdullah Al Mahmud",
+   designation: "Associate Software Developer",
+   company: "IQVIA",
+   overview: "",
+};
+
+export const experience = [
+   {
+      role: "Associate Software Developer",
+      company: "IQVIA",
+      timeSpan: "August 2021 - Present",
+      responsibilities: "",
+   },
+];
+
+export const skills = {
+   backEnd: ["Java", ".NET", "Python"],
+   backEndFrameworkOrLibrary: ["LINQ", "NUnit", "Entity Framework"],
+   frontEnd: ["HTML", "CSS", "Bootstrap", "TailwindCSS"],
+   frontEndFrameworkOrLibrary: ["React", "Material UI", "Google Charts", "Apex Charts"],
+   database: ["MySQL", "MS SQL Server"],
+   versionControl: [
+      "Git",
+      "GitHub",
+      "TortoiseGit",
+      "Subversion",
+      "TortoiseSVN"
+   ],
+   projectManagement: ["JIRA"],
+   dataVisualisation: ["Power BI"],
+   officeTools: ["MS Word", "MS Excel", "MS Powerpoint"],
+};
+
+export const certifications = [];
+export const education = [];
+export const projects = [];
+export const contacts = [
+   {
+      link: "mailto:mahmudmridul.185@gmail.com",
+      img: "gmail.png"
+   },
+   {
+      link: "https://www.linkedin.com/in/abdullahalmahmudmridul/",
+      img: "linkedin.png"
+   }
+];
+
+// "certifications": [
+//    {
+//       "problemSolving": "Problem solving (Basic) [HackerRank]",
+//       "link": "https://www.hackerrank.com/certificates/91e7e3042b00"
+//    },
+//    {
+//       "javaBasic": "Java (Basic) [HackerRank]",
+//       "link": "https://www.hackerrank.com/certificates/599036d179e6"
+//    },
+//    {
+//       "javaEE": "Introduction to Java EE [Udemy]",
+//       "link": "https://www.udemy.com/certificate/UC-243c8565-f318-44cd-b5a0-c41c7a3c2857/"
+//    },
+//    {
+//       "entityFramework": "Introduction to Entity Framework [LinkedIn Learning]"
+//    },
+//    {
+//       "sql": "Advanced SQL for Data Scientists (2017) [LinkedIn Learning]"
+//    }
+// ],
+//    "education": [
+//       {
+//          "degree": "Bachelor of Science",
+//          "institution": "BRAC University",
+//          "major": "Computer Science & Engineering",
+//          "cgpa": 3.6,
+//          "time": "2017 - 2021"
+//       },
+//       {
+//          "degree": "HSC",
+//          "institution": "Govt. Science College",
+//          "cgpa": 5.0,
+//          "time": "2014 - 2016"
+//       },
+//       {
+//          "degree": "SSC",
+//          "institution": "Govt. Science School",
+//          "cgpa": 5.0,
+//          "time": "2004 - 2013"
+//       }
+//    ],
