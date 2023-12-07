69de3959bfd44b819e3e8743f7bed0d6315287b1
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-12-07 17:01:06 +0600

    Add shadow

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ExperienceList from './sections/experience/ExperienceList';
 // change tailwind from dev dependencies to dependencies
 export default function App() {
    return (
-      <main className="container max-w-5xl p-4 font-nunito bg-slate-100">
+      <main className="container max-w-5xl p-4 font-nunito bg-slate-100 shadow-2xl">
          <Overview />
          <ExperienceList />
       </main>
