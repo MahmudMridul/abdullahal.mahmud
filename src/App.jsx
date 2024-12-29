a846d62e199468654c379171fc4bc5ecc97941bf
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-29 13:53:42 +0600

    style: background and text color

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-	return <div className="font-nunito container mx-auto p-3"></div>;
+	return (
+		<div className="font-nunito container mx-auto p-3 bg-neutral-900 text-gray-50">
+			app
+		</div>
+	);
 }
