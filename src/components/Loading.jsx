bde8c950b7c1d2f73eb1f10d0771b038bd2ca095
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-29 15:52:00 +0600

    style: add loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="font-nunito bg-neutral-900 text-gray-50 text-4xl h-screen flex justify-center items-center">
+			Loading...
+		</div>
+	);
+}
