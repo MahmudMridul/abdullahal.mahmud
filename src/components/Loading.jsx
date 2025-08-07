a446e2321cb9b85e30c3e961ac4bf2e7dadfa8b5
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-08-08 00:42:39 +0600

    Fresh start with html

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-	return (
-		<div className="font-nunito bg-neutral-900 text-gray-50 text-4xl h-screen flex justify-center items-center">
-			Loading...
-		</div>
-	);
-}
